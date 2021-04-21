febff17a77d049da09e4b8a549ed17d2bb4f1a66
Nag3156 <s.n.reddy3156@gmail.com>
2021-04-21 13:35:18 +0530

    first

---
+++ reservation_management_service/src/main/java/com/room/controller/restcontroller.java
@@ -0,0 +1,52 @@
+package com.room.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.room.model.reservation;
+import com.room.service.reservationService;
+
+
+@RestController
+public class restcontroller {
+	
+	@Autowired
+	private reservationService service;
+	
+	@PostMapping("/rooms")
+	@Transactional
+	public String registerRoom(@RequestBody reservation Reservation) {
+		service.saveMyRoom(Reservation);
+		return "Hello "+Reservation.getRoomno()+ " room has Allocated";
+		
+	}
+	
+	@GetMapping("/rooms")
+	public Iterable<reservation> showAllrooms(){
+		return service.showAllrooms();
+	}
+	
+	@DeleteMapping("/rooms/{roomno}")
+	public Iterable<reservation> deleteRoom(@PathVariable String roomno){
+		return service.deleteByRoomno(roomno);
+	}
+	
+	@GetMapping("/rooms/{roomno}")
+	public reservation searchRoomno(@PathVariable String roomno) {
+		return service.findByRoomno(roomno);
+	}
+	
+	@PutMapping("/rooms")
+	public String updateroom(@RequestBody reservation Reservation) {
+		return service.updateRoom(Reservation);
+	}
+
+
+}
